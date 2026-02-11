3a5c0b1522d9484ec29ce4149b523d8dba659f1d
hannah <jyebe9034@gmail.com>
2026-02-11 17:05:08 +0900

    practice some DOM manipulate functions

--- js/practice.js
+++ js/practice.js
@@ -24,3 +24,44 @@ div.classList.remove("new");
 div.classList.toggle("active");
 
 div.textContent = "Hello World";
+
+// select anotherContainer div
+const anotherContainer = document.querySelector("#anotherContainer");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is a new text-content!";
+
+anotherContainer.appendChild(content);
+
+// <p style="color: red;">Hey, I'm red!</p>
+// <h3 style="color: blue;">I'm a blue h3!</h3>
+// <div style="border-color: black; background-color: pink;"></div>
+
+const newContainer = document.querySelector("#newContainer");
+
+const p = document.createElement("p");
+p.setAttribute("style", "color: red;");
+p.textContent = "Hey, I'm red!";
+
+const h3 = document.createElement("h3");
+h3.setAttribute("style", "color: blue;");
+h3.textContent = "I'm a blue h3!";
+
+const newDiv = document.createElement("div");
+newDiv.setAttribute("id", "newDiv");
+newDiv.setAttribute("style", "border-color: black; background-color: pink;");
+
+const newH1 = document.createElement("h1");
+newH1.textContent = "I'm in a div!"
+
+const anotherP = document.createElement("p");
+anotherP.textContent = "ME TOO!";
+
+newDiv.appendChild(newH1);
+newDiv.appendChild(anotherP);
+
+newContainer.appendChild(p);
+newContainer.appendChild(h3);
+newContainer.appendChild(newDiv);
+
